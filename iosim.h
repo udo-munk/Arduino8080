fb5fe52582feed3469bc8789e06d32e22ee6e791
Udo Munk <udo.munk@freenet.de>
2024-04-17 20:28:18 +0200

    implemented read from tty port, needs example to test

--- iosim.h
+++ iosim.h
@@ -4,9 +4,29 @@
 // Copyright 2024, Udo Munk
 //
 
+// I/O function port 1 read:
+// Read byte from Arduino tty.
+static BYTE p001_in(void)
+{
+  while (!Serial.available())   // block until data available
+    ;                           // compatible with z80sim
+  return((BYTE)Serial.read());  // read data
+}
+
+// This array contains function pointers for every input
+// I/O port (0 - 255), to do the required I/O.
+const static BYTE (*port_in[5]) (void) = {
+  0,                      // port 0
+  p001_in                 // port 1
+};
+
 // read a byte from 8080 CPU I/O
 static BYTE io_in(BYTE addrl, BYTE addrh)
 {
+  if (addrl == 1) // for now we can use 1 only
+    return((*port_in[addrl]) ());
+  else
+    return(0xff);
 }
 
 // I/O function port 1 write:
